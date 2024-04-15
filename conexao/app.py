35a4fdde472d0d72e600bc212eddc9cd178f646b
playerking3 <123572431+playerking3@users.noreply.github.com>
2024-04-15 11:15:13 -0300

    quase pronto

--- conexao/app.py
+++ conexao/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select, union_all
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/cashflow'
@@ -10,6 +11,7 @@ class Receitas(db.Model):
     data = db.Column(db.Date)
     valor = db.Column(db.Integer)
     descricao = db.Column(db.String(150))
+  #  id_usuario(db.Column(db.foreng_key (Usuario.id_usuario))
 
 class Despesas(db.Model):
     id_despesa = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -25,13 +27,20 @@ def entrar():
 def geral():
     return render_template('Geral.html')
 
+
 @app.route('/listagem')
 def listagem():
+    # Crie as seleções SQL para cada consulta
+    selecao_despesas = Despesas.query.all()
+    selecao_receitas = Receitas.query.all()
+    dados = []
+    for receita in selecao_receitas:
+        dados.append(receita)
 
-    ListaDespesas = Despesas.query.all()
-    ListaReceitas = Receitas.query.all()
+    for despesa in selecao_despesas:
+        dados.append(despesa)
 
-    return render_template('Listagem.html', despesas=ListaDespesas, receitas=ListaReceitas)
+    return render_template('Listagem.html', despesas=selecao_despesas, receitas=selecao_receitas, tudo=dados)
 
 @app.route('/config')
 def config():
