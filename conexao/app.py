94bbbf03e0d46f267fe4268612de1a9dbe71549d
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-04-15 14:14:58 -0300

    cadastro usuario feito

--- conexao/app.py
+++ conexao/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import generate_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/cashflow'
+app.config['SECRET_KEY'] = 'chave'
 db = SQLAlchemy(app)
 
 class Receitas(db.Model):
@@ -17,12 +19,18 @@ class Despesas(db.Model):
     valor = db.Column(db.Integer)
     descricao = db.Column(db.String(150))
 
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome= db.Column(db.String(50))
+    email= db.Column(db.String(100))
+    senha= db.Column(db.String(50))
+
 @app.route('/')
 def entrar():
     return render_template('Entrar.html')
 
-@app.route('/')
-def entrar():
+@app.route('/login')
+def login():
     return render_template('Login.html')
 
 @app.route('/geral')
@@ -41,9 +49,26 @@ def listagem():
 def config():
     return render_template('Config.html')
 
+@app.route('/cadastrolista')
+def cadastrolista():
+    return render_template('CadastroListagem.html')
+
 @app.route('/cadastro')
 def cadastro():
-    return render_template('CadastroListagem.html')
+    return render_template('Cadastro.html')
+@app.route('/criar_user', methods=['POST'])
+def criar_user():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+
+    user = Usuario.query.filter_by(email=email).first()
+    senha_hash = generate_password_hash(senha).decode('utf-8')
+    novo_usuario = Usuario(nome=nome, email=email, senha=senha_hash)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    flash('Usuário cadastrado com sucesso', 'success')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
